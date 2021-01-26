26e8b850988c98a0e2293d2881c16070b229b36b
Cody Hatfield <hatfield.coders@gmail.com>
2021-01-26 00:35:58 -0500

    [AVL-13] Refactored VehicleManager

--- Unity-AVL/Assets/Scripts/Simulation/VehicleManager.cs
+++ Unity-AVL/Assets/Scripts/Simulation/VehicleManager.cs
@@ -8,10 +8,10 @@ public class VehicleManager : MonoBehaviour
     [SerializeField] protected List<string> carNameList = new List<string>();
 
     protected Dictionary<string, VehicleBase> activeVehicles = new Dictionary<string, VehicleBase>();
+
     protected VehicleBase vehicleBuffer = null;
-    protected VehicleUpdateData updateVehicleBuffer = null;
+    protected VehicleUpdateData updateBuffer = null;
     protected VehicleState stateBuffer = null;
-    protected GameObject objectBuffer = null;
     protected Vector3 vector3Buffer = new Vector3();
     protected Vector2 positionOffset = new Vector2();
 
@@ -27,34 +27,34 @@ public class VehicleManager : MonoBehaviour
         string[] dataPerVehicle = rawData.Split(scr_TCP.DATA_DELIM);
 
         for (int i = 0; i < dataPerVehicle.Length; i++) {
-            this.updateVehicleBuffer = JsonUtility.FromJson<VehicleUpdateData>(dataPerVehicle[i]);
+            this.updateBuffer = JsonUtility.FromJson<VehicleUpdateData>(dataPerVehicle[i]);
 
-            if (this.updateVehicleBuffer.vehicleId == null) {
+            if (this.updateBuffer.vehicleId == null) {
                 Debug.LogError("Vehicle data received from SUMO which lacks a proper vehicle ID.");
                 continue;
             }
 
-            if (!this.activeVehicles.ContainsKey(this.updateVehicleBuffer.vehicleId)) {
+            if (!this.activeVehicles.ContainsKey(this.updateBuffer.vehicleId)) {
 
-                Debug.LogError("Vehicle ID [" + this.updateVehicleBuffer.vehicleId + "] was not found in the dictionary of active vehicles.");
+                Debug.LogError("Vehicle ID [" + this.updateBuffer.vehicleId + "] was not found in the dictionary of active vehicles.");
                 continue;
 
             }
 
-            this.vehicleBuffer = this.activeVehicles[this.updateVehicleBuffer.vehicleId];
+            this.vehicleBuffer = this.activeVehicles[this.updateBuffer.vehicleId];
 
             this.vector3Buffer = this.vehicleBuffer.transform.position;
-            this.vector3Buffer.x = (float)(this.updateVehicleBuffer.position[0] + this.positionOffset.x);
-            this.vector3Buffer.z = (float)(this.updateVehicleBuffer.position[1] + this.positionOffset.y);
+            this.vector3Buffer.x = (float)(this.updateBuffer.position[0] + this.positionOffset.x);
+            this.vector3Buffer.z = (float)(this.updateBuffer.position[1] + this.positionOffset.y);
 
             this.vehicleBuffer.transform.position = this.vector3Buffer;
-            this.vehicleBuffer.transform.rotation = Quaternion.AngleAxis(this.updateVehicleBuffer.heading, Vector3.up);
+            this.vehicleBuffer.transform.rotation = Quaternion.AngleAxis(this.updateBuffer.heading, Vector3.up);
 
             this.stateBuffer = this.vehicleBuffer.GetVehicleState();
-            this.stateBuffer.SetHeading(this.updateVehicleBuffer.heading);
-            this.stateBuffer.SetSpeed(this.updateVehicleBuffer.speed);
+            this.stateBuffer.SetHeading(this.updateBuffer.heading);
+            this.stateBuffer.SetSpeed(this.updateBuffer.speed);
             this.stateBuffer.SetTimer(Time.fixedDeltaTime);
-            this.stateBuffer.SetIsBraking(this.updateVehicleBuffer.brake);
+            this.stateBuffer.SetIsBraking(this.updateBuffer.brake);
 
             this.vehicleBuffer.UpdateState();
         }
