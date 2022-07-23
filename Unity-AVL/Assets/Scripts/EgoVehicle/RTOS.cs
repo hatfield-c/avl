76028c2291d5b2ead87f4802611844949d787eb3
Cody Hatfield <hatfield.coders@gmail.com>
2022-07-23 02:56:42 -0500

    [AVL-29] Task scheduler has been implemented

---
+++ Unity-AVL/Assets/Scripts/EgoVehicle/RTOS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTOS : MonoBehaviour
+{
+    [SerializeField]
+    protected List<string> tasks = new List<string>();
+
+    protected int taskIndex = 0;
+
+    void FixedUpdate()
+    {
+        if(this.taskIndex >= this.tasks.Count) {
+            this.taskIndex = 0;
+        }
+
+        string task = this.tasks[this.taskIndex];
+        Debug.Log("Executing task: " + task);
+
+        this.taskIndex++;
+    }
+}
