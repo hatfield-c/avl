a9b6da70ffa4b8e7d70f53228a6d4771252e1486
Cody Hatfield <hatfield.coders@gmail.com>
2022-07-24 21:54:47 -0500

    [AVL-31] Directional compass sensor has been added

---
+++ Unity-AVL/Assets/Scripts/Sensors/DirectionSensor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionSensor : AbstractSensor
+{
+    [SerializeField]
+    protected Transform egoVehicle = null;
+
+    [SerializeField]
+    protected Transform spinner = null;
+
+    [SerializeField]
+    protected Vector3 targetDirection = Vector3.forward;
+
+    override public byte[] ReadSensor(byte option) {
+        byte[] sensorData;
+
+        float angle = Vector3.SignedAngle(this.targetDirection, this.egoVehicle.transform.forward, Vector3.up);
+        sensorData = System.BitConverter.GetBytes(angle);
+
+        return sensorData;
+    }
+
+    void FixedUpdate() {
+        this.spinner.LookAt(this.spinner.position + this.targetDirection.normalized);
+    }
+}
