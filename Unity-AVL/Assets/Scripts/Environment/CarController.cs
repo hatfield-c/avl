3dca38f11148cf6d08e3e5ad0462aa950fdedb64
Cody Hatfield <hatfield.coders@gmail.com>
2022-08-10 23:08:12 -0500

    [AVL-40] Obstacle cars now randomly backup

--- Unity-AVL/Assets/Scripts/Environment/CarController.cs
+++ Unity-AVL/Assets/Scripts/Environment/CarController.cs
@@ -5,10 +5,36 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     [SerializeField]
+    protected Transform egoVehicle = null;
+
+    [SerializeField]
     protected Transform carContainer = null;
 
     [SerializeField]
     protected List<Material> carMaterials = null;
+
+    [SerializeField]
+    protected float minTime = 0f;
+
+    [SerializeField]
+    protected float maxTime = 0f;
+
+    [SerializeField]
+    protected bool isCarsActive = false;
+
+    protected float timer = 0f;
+
+    void FixedUpdate() {
+        if (!this.isCarsActive) {
+            return;
+        }
+
+        this.timer -= Time.fixedDeltaTime;
+
+        if(this.timer <= 0) {
+            this.MoveCar();
+        }
+    }
 
     void Start()
     {
@@ -19,10 +45,27 @@ public class CarController : MonoBehaviour
             carObstacle.SetMaterial(randomMat);
         }
 
-        int emptyIndex = Random.Range(0, this.carContainer.childCount);
-        Transform emptySpace = this.carContainer.GetChild(emptyIndex);
-        emptySpace.parent = null;
+        CarObstacle emptySpace = this.GetRandomCar();
+        emptySpace.transform.parent = null;
         Destroy(emptySpace.gameObject);
+
+        if (this.isCarsActive) {
+            this.MoveCar();
+        }
+    }
+
+    protected void MoveCar() {
+        this.timer = Random.Range(this.minTime, this.maxTime);
+
+        CarObstacle car = this.GetRandomCar();
+        car.Move(this.egoVehicle);
+    }
+
+    protected CarObstacle GetRandomCar() {
+        int index = Random.Range(0, this.carContainer.childCount - 1);
+        Transform carTransform = this.carContainer.GetChild(index);
+
+        return carTransform.gameObject.GetComponent<CarObstacle>();
     }
 
     protected Material GetRandomMat() {
--- Unity-AVL/Assets/Scripts/Environment/CarObstacle.cs
+++ Unity-AVL/Assets/Scripts/Environment/CarObstacle.cs
@@ -4,11 +4,93 @@ using UnityEngine;
 
 public class CarObstacle : MonoBehaviour
 {
+    protected enum CarState {
+        waiting,
+        backward,
+        forward
+    };
+
     [SerializeField]
     protected MeshRenderer bodyRenderer = null;
 
     [SerializeField]
     protected MeshRenderer topRenderer = null;
+
+    [SerializeField]
+    protected float egoDistance = 0f;
+
+    [SerializeField]
+    protected float egoAngle = 0f;
+
+    [SerializeField]
+    protected float maxDistance = 0f;
+
+    [SerializeField]
+    protected float speed = 0f;
+
+    protected Transform egoVehicle = null;
+    protected Vector3 originalPosition = new Vector3();
+    protected CarState currentState = CarState.waiting;
+    protected float traveled = 0f;
+
+    void Start() {
+        this.originalPosition = this.transform.position;
+    }
+
+    void FixedUpdate() {
+        if(this.currentState == CarState.waiting) {
+            return;
+        }
+
+        if (this.currentState == CarState.forward) {
+            this.transform.position += this.transform.forward * this.speed;
+
+            this.traveled += this.speed;
+
+            if (this.traveled >= this.maxDistance) {
+                this.currentState = CarState.waiting;
+                this.transform.position = this.originalPosition;
+                this.traveled = 0;
+            }
+
+            return;
+        }
+
+        if (this.currentState == CarState.backward) {
+            Vector3 egoDifference = this.egoVehicle.position - this.transform.position;
+            float egoAlignment = Vector3.Angle(-this.transform.forward, egoDifference);
+            
+            if(egoDifference.magnitude <= this.egoDistance && egoAlignment <= this.egoAngle) {
+                this.currentState = CarState.forward;
+
+                Vector3 originDifference = this.originalPosition - this.transform.position;
+                this.traveled = this.maxDistance - originDifference.magnitude;
+
+                return;
+            }
+
+            this.transform.position += this.transform.forward * -this.speed;
+
+            this.traveled += this.speed;
+
+            if (this.traveled >= this.maxDistance) {
+                this.currentState = CarState.forward;
+                this.traveled = 0;
+            }
+
+            return;
+        }
+
+    }
+
+    public void Move(Transform egoVehicle) {
+        if(this.currentState != CarState.waiting) {
+            return;
+        }
+
+        this.egoVehicle = egoVehicle;
+        this.currentState = CarState.backward;
+    }
 
     public void SetMaterial(Material material) {
         this.bodyRenderer.material = material;
